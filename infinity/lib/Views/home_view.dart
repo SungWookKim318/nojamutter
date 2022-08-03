3b5aedfdabc532d7c83c5b85ecd68146735bc244
SungWook Kim <sungwook.kim318@gmail.com>
2022-08-03 17:05:30 +0900

    home_view 추가

---
+++ infinity/lib/Views/home_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: createScaffold(context));
+  }
+
+  Scaffold createScaffold(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[const Text("Test My Home View")],
+      )),
+      floatingActionButton: FloatingActionButton(
+          onPressed: _pressPush, child: const Icon(Icons.add)),
+    );
+  }
+
+  void _pressPush() {
+    debugPrint("press Push button");
+  }
+}
--- infinity/lib/main.dart
+++ infinity/lib/main.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:infinity/Views/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,50 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+    return const MaterialApp(title: 'Startup Name Generator', home: HomeView());
   }
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ));
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => RandomWordsState();
 }
