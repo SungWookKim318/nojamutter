3c5288496cdac3ed8914d21eb7f77f80a10ed3de
SungWook Kim <sungwook.kim318@gmail.com>
2022-08-12 14:26:05 +0900

    iOS Foreground에서도 푸시 나오도록 수정

--- infinity/lib/ViewModels/notification_viewmodel.dart
+++ infinity/lib/ViewModels/notification_viewmodel.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:infinity/Managers/notification_manager.dart';
 
 class NotificationViewModel {
-  static bool _isInit = false;
-
   static Future<bool> initializeNotification() async {
     if (NotificationManager().isInit == true) {
       debugPrint("already initaiozied notification manager");
